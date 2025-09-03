f5543144d37f9195882f8782fcad7e57b7934f5a
Hariom Mandal <hariommandal15@gmail.com>
2025-09-03 20:38:45 +0530

    rest-api

--- 07-express/01-restApi/index.js
+++ 07-express/01-restApi/index.js
@@ -1,23 +1,14 @@
-const express = require('express');
-const users = require("./example.json");
+const express = require("express")
+const users = require("./example.json")
 
-const app = express();
-const port = 3000;
+const app = express()
 
-app.get('/api/users', (req, res) => {
-    return res.json(users);
-});
+// Routes
+app.get('/users', (req, res) => {
+    res.json(users)
+})
 
-app.get('/api/users/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id); 
-    return res.json(user);
-});
-
-app.patch('/api/users/:id', (req, res) => {
-    return res.end({status : "pending"})
-});
-
-app.listen(port, () => {
-    console.log(`Check your first project at http://localhost:${port}`);
-});
+app.listen(3000,() => {
+    console.log("hi there");
+    
+})
